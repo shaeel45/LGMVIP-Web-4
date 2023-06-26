d76e610a37b142c349475cb5fd65c90300e6820b
shaeel45 <beingshaeel@gmail.com>
2023-06-26 16:25:01 +0500

    all set

--- cal.js
+++ cal.js
@@ -1,24 +1,19 @@
 let string = "";
 let buttons = document.querySelectorAll(".btn");
-Array.from(buttons).forEach((buttons)=> {
-    buttons.addEventListener('click', (event)=>{
-        if(event.target.innerHTML == '='){
-            string=eval(string);
-            document.querySelector('input').value = string;
-        }
-        else if(event.target.value == ".") {
-            string= eval(string);
-            document.querySelector('input').value = string;
-        }
-        else if(event.target.innerHTML == 'C'){
-            string = ""
-            document.querySelector('input').value = string;
-        }
-        else{
-            console.log(event.target)
-            string = string + event.target.innerHTML;
-            document.querySelector('input').value = string;
+Array.from(buttons).forEach((button) => {
+    button.addEventListener('click', (event) => {
+        const value = event.target.innerHTML.trim(); 
 
+        if (value === '=') {
+            string = eval(string);
+            document.querySelector('input').value = string;
+        } else if (value === 'C') {
+            string = "";
+            document.querySelector('input').value = string;
+        } else {
+            console.log(event.target);
+            string += value;
+            document.querySelector('input').value = string;
         }
-    })
+    });
 });
